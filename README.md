fe939302c7ee5bd14a0282161710e19604b6c147
Michael <icuemike@outlook.com>
2021-03-14 05:47:24 -0400

    Added updated files

---
+++ README.md
@@ -0,0 +1,11 @@
+## Tools
+
+- You will need to be sure to install [NodeJS](Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. "NodeJS") which at this time the latest is: `14.16.0 LTS Recommended` or download **Windows (Chocolatey)** using SHELL: `choco install nodejs-lts`
+- Be sure to have the latest coding IDE installed such as [VSCode](https://code.visualstudio.com/ "VSCode") which is free.
+
+## Download Files
+
+Download the latest files from here to get started.
+
+## What Next?
+Open up the scrapers.js file and read the comments to help get you started. Do not be affraid to break this as if you are learning, you will break it, but I will be abled to help by answering your questions. To get started, you should create an [**issue**](https://github.com/icueMike/Web-Scrapping-Puppeteer/issues/new/choose "issue").
--- scrapers.js
+++ scrapers.js
@@ -6,23 +6,29 @@ async function scrapeBuilds(url) {
     const page = await browser.newPage();
     await page.goto(url);
 
+    // Lines 10-12 is for the image before the title or you can swap them around to however you'd like.
+    // You will need to get the src element @id="landingImage on line 11 and replace it by copying the Xpath. Do not copy Full XPath unless this line is giving you problems.
     const [el] = await page.$x('//*[@id="landingImage"]');
     const src = await el.getProperty('src');
     const srcTxt = src.jsonValue();
 
+    // el2 is the second element which can be used for a title as seen on line 16. Replace [@id="productTitle"] just like you did on line 11
     const [el2] = await page.$x('//*[@id="productTitle"]');
     const txt2 = await el2.getProperty('textContent');
     const rawTxt2 = await txt2.jsonValue();
 
+    // el3 is the second element which can be used for a content as seen on line 16. Replace [@id="certified-refurbished-version"]/div/div/div[2]/span[1] just like you did on line 11 & & 16
     const [el3] = await page.$x('//*[@id="certified-refurbished-version"]/div/div/div[2]/span[1]');
     const txt3 = await el3.getProperty('textContent', '<p>');
     const rawTxt3 = await txt3.jsonValue();
 
     console.log({srcTxt, rawTxt2, rawTxt3});
 
-
     browser.close();
 
 }
 
+// Replce the URL with the url to the page you are scraping from
 scrapeBuilds('https://www.amazon.com/GIGABYTE-B450M-DS3H-Ryzen-Motherboard/dp/B07FWVJSHC/?_encoding=UTF8&pd_rd_w=Ub8Ik&pf_rd_p=de0c3fe6-321f-473e-bef6-6a700af423d3&pf_rd_r=6HGSJT676F1X97XRBKM9&pd_rd_r=23f5ea35-a011-46c1-9b1b-7b49b592cb0c&pd_rd_wg=opke0&ref_=pd_gw_trq_rep_sims_gw');
+
+// Follow my twitter account at https://twitter.com/icuemike for quesitons and answers or post in the issues section here
